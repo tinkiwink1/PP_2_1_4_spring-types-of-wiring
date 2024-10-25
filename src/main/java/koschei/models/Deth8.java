84a8df5753d77dc6add48eca5beb5b0516718806
Ildar Gimadutdinov <tinkiwinki1443@gmail.com>
2024-10-25 22:46:00 +0400

    add beans in classes

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,10 @@
 package koschei.models;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    @Override
+    public String toString() { return "";}
 }
